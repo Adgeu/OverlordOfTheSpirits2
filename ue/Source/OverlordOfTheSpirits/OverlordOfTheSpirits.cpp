0298f2e62172d68d1255f3b3498243272e1befa2
Alexey Faradzhev <adgeu@mail.ru>
2022-06-23 19:17:23 +0300

    Initial commit

---
+++ ue/Source/OverlordOfTheSpirits/OverlordOfTheSpirits.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OverlordOfTheSpirits.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OverlordOfTheSpirits, "OverlordOfTheSpirits" );
